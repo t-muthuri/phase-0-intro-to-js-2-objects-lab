39481abf9d556bfa6a3a895413bd39dfa0701864
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-08-12 12:43:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Write your solution in this file!
+const employee = {
+    name: "Andy",
+    streetAddress: "Hse 10 Buru",
+};
+
+function updateEmployeeWithKeyAndValue (){
+    return {
+        ...employee,
+        ["name"]:"Sam",
+        ["streetAddress"]:"11 Broadway",
+    };
+};
+
+function beforeEach (){
+    employee.length = 0
+    employee.push(
+        "name"["Andy"],
+        "streetAddress"["Hse 10 Buru"],
+    );
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+    employee ["name"] = "Sam";
+    employee ["streetAddress"] = "12 Broadway";
+    return employee;
+}
+
+function beforeEach1 (){
+    employee.length = 0
+    employee.push(
+        "name"["Andy"],
+        "streetAddress"["Hse 10 Buru"],
+    );
+}
+
+const newEmployee = {
+    name : undefined,
+};
+
+function deleteFromEmployeeByKey(){
+        delete newEmployee ["name"];
+        return newEmployee;
+}
+
+function beforeEach2 (){
+    employee.length = 0
+    employee.push(
+        "name"["Andy"],
+        "streetAddress"["Hse 10 Buru"],
+    );
+    }
+
+function destructivelyDeleteFromEmployeeByKey(){
+    delete employee.name;
+    return employee;
+}
